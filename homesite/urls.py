8296d6c22fafd2b8c5a4e2e345acac40fb84d9d2
aninternetof <bradyhurlburt@gmail.com>
2016-11-23 14:15:43 -0500

    Adds logout

--- homesite/urls.py
+++ homesite/urls.py
@@ -20,5 +20,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
-	url(r'', include('blog.urls'))
+    url(r'^login/$', django.contrib.auth.views.login, name='login_shortcut'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls'))
 ]
